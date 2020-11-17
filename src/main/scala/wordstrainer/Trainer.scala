ceb80e7d46447753f1199ffc39aa1e434f1cbe91
Anton Ivanov <aivanov@uber.com>
2020-11-17 11:12:11 +0000

    do not accept unknown answers

--- src/main/scala/wordstrainer/Trainer.scala
+++ src/main/scala/wordstrainer/Trainer.scala
@@ -7,13 +7,19 @@ private object Trainer {
     var i = 0
     for (w <- words) {
       // TODO: add color
-      // TODO: remove
       print('\n' + w.question)
       Console.in.readLine()
 
-      print(w.answer + " y/n? ")
-      val answer = Console.in.readLine()
-      res(i) = answer == "y" || answer == "Y"
+      print(w.answer)
+
+      var answer: String = null
+      do {
+        print( " y/n? ")
+        answer = Console.in.readLine()
+      } while (answer != "y" && answer != "n")
+
+      res(i) = answer == "y"
+
       i += 1
     }
     res
